20e8dce1cde60546c985056c1cd09bffd1f68c17
caimelo <caio.melo51@gmail.com>
2025-06-17 15:17:13 -0300

    Feat: Adaptar SalaRepositoryImpl para usar Room e mapeadores

--- app/src/main/java/com/example/checklistsalas/data/repository/SalaRepositoryImpl.kt
+++ app/src/main/java/com/example/checklistsalas/data/repository/SalaRepositoryImpl.kt
@@ -1,26 +1,30 @@
 package com.example.checklistsalas.data.repository
 
+import com.example.checklistsalas.data.local.dao.SalaDao
+import com.example.checklistsalas.data.local.mapper.toSala
+import com.example.checklistsalas.data.local.mapper.toSalaEntity
 import com.example.checklistsalas.domain.model.Sala
 import com.example.checklistsalas.domain.repository.SalaRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class SalaRepositoryImpl : SalaRepository{
-    private val salas =
-        MutableStateFlow(listOf(
-            Sala(1, "Sala 101","LQ", "1 andar", "SP"),
-            Sala(2, "Sala 202", "EG", "2 andar", "RJ"),
-            Sala(3, "Sala 303", "VM22", "3 andar", "SP")
-        ))
+class SalaRepositoryImpl @Inject
+constructor(
+    private val salaDao: SalaDao
+) : SalaRepository {
 
     override fun getSalas(): Flow<List<Sala>> {
-        return salas.asStateFlow()
+        return salaDao.getSalas().map { entityList ->
+            entityList.map {
+                it.toSala()
+            }
+        }
     }
 
     override suspend fun atualizarSala(sala: Sala) {
-        salas.value = salas.value.map {
-            if(it.id == sala.id) sala else it
-        }
+        salaDao.atualizarSala(sala.toSalaEntity())
     }
 }
